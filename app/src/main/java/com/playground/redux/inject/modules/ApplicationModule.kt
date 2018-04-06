9e57dbffebeb405d2d16ad805db9efcdc774de02
Peter Nagy <peter_istvan_nagy@epam.com>
2018-04-06 16:55:06 +0200

    Null handling in the reducer

--- app/src/main/java/com/playground/redux/inject/modules/ApplicationModule.kt
+++ app/src/main/java/com/playground/redux/inject/modules/ApplicationModule.kt
@@ -1,6 +1,7 @@
 package com.playground.redux.inject.modules
 
 import com.playground.redux.appstate.AppState
+import com.playground.redux.appstate.GithubUserState
 import com.playground.redux.middlewares.githubUserMiddleware
 import com.playground.redux.reducer.appReducer
 import dagger.Module
@@ -13,8 +14,8 @@ class ApplicationModule {
 
     @Singleton
     @Provides
-    fun provideStore() : Store<AppState> {
-        return Store(reducer = ::appReducer, state = null, middleware = listOf(githubUserMiddleware))
+    fun provideStore(): Store<AppState> {
+        return Store(reducer = ::appReducer, state = AppState(GithubUserState()), middleware = listOf(githubUserMiddleware))
     }
 
 }
