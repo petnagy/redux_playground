aad6cd615b5a691c531a5b3d6100c1550cdfcf56
Peter Nagy <petnagy@gmail.com>
2018-04-19 22:15:18 +0200

    Add State type to CommitViewState

--- app/src/main/java/com/playground/redux/appstate/CommitState.kt
+++ app/src/main/java/com/playground/redux/appstate/CommitState.kt
@@ -1,5 +1,6 @@
 package com.playground.redux.appstate
 
 import com.playground.redux.data.GitCommit
+import tw.geothings.rekotlin.StateType
 
-data class CommitState(val commitList: List<GitCommit> = emptyList(), val loading: Boolean = false)
+data class CommitState(val commitList: List<GitCommit> = emptyList(), val loading: Boolean = false): StateType
